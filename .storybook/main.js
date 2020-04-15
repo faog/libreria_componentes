0aec453ab1bfef691dabc5680bca7dab3cda66cb
faog <fabiola.orellana.g@gmail.com>
2020-04-15 17:29:19 -0400

    feat: configuración para personalizar el theme de storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,7 @@
 module.exports = {
-    stories: ['../src/**/**/*.stories.js'],
-    addons: ['@storybook/addon-actions', '@storybook/addon-links'],
-    webpackFinal: async config => {
-      // do mutation to the config
-  
-      return config;
-    },
-  };
+  stories: ['../src/**/**/*.stories.js'],
+  addons: ['@storybook/addon-actions', '@storybook/addon-links'],
+  webpackFinal: async config => {
+    return config;
+  },
+};
